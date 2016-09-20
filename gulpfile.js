a1014588eee26a48b1adbb521dcc1fbf9a52fbcb
Richard Cornelissen <mail@richardcornelissen.com>
2016-09-20 21:35:28 +0200

    fix large files

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,13 @@
 // Sass configuration
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var cleanCSS = require('gulp-clean-css');
-var sourcemaps = require('gulp-sourcemaps');
 var rename = require('gulp-rename');
 
 
 gulp.task('sass', function() {
     gulp.src('css/**/*.scss')
-        .pipe(sass())
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({suffix: '.min'}))
-        .pipe(sourcemaps.init())
-        .pipe(cleanCSS())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(function(f) {
-            return f.base;
-        }))
-});
-
-gulp.task('css-minify', function(){
-    gulp.src(['css/**/*.css', '!css/**/*.min.css'])
-        .pipe(rename({suffix: '.min'}))
-        .pipe(cleanCSS())//TODO sourcemaps
         .pipe(gulp.dest(function(f) {
             return f.base;
         }))
@@ -29,5 +15,4 @@ gulp.task('css-minify', function(){
 
 gulp.task('default', ['sass'], function() {
     gulp.watch('css/**/*.scss', ['sass']);
-    gulp.watch(['css/**/*.css', '!css/**/*.min.css'], ['css-minify']);
 })
